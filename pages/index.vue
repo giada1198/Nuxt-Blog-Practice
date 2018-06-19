c35cd9dda237e9915875d24e18e8af97dc56ec11
Giada Sun <syuanchs@andrew.cmu.edu>
2018-06-19 21:45:43 +0800

    install pug & sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,14 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        giada-website
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+section.container
+  div
+    app-logo
+      h1.title
+        | giada-website
+      h2.subtitle
+        | Nuxt.js project
+      .links
+        a.button--green(href='https://nuxtjs.org/', target='_blank') Documentation
+        a.button--grey(href='https://github.com/nuxt/nuxt.js', target='_blank') GitHub
 </template>
 
 <script>
@@ -32,33 +21,29 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass">
+.container
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.title
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
+  display: block
+  font-weight: 300
+  font-size: 100px
+  color: #35495e
+  letter-spacing: 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.subtitle
+  font-weight: 300
+  font-size: 42px
+  color: #526488
+  word-spacing: 5px
+  padding-bottom: 15px
 
-.links {
-  padding-top: 15px;
-}
+.links
+  padding-top: 15px
 </style>
