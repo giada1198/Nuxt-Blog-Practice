c4351e6141954792d00948be7cdbbe7f55b1cd86
Giada Sun <syuanchs@andrew.cmu.edu>
2018-06-21 01:53:43 +0800

    fix router

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,9 @@ module.exports = {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900'}
     ]
   },
-  // router: {
-  //   base: '/giada-website/'
-  // },
+  router: {
+    base: '/giada-website/'
+  },
   /*
   ** Customize the progress bar color
   */
